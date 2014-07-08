f806b26cd6402f102550228296db5ec22a905e97
icejoywoo <icejoywoo@gmail.com>
2014-07-08 20:46:20 +0800

    characters encodings

--- src/main/java/HelloJava.java
+++ src/main/java/HelloJava.java
@@ -1,8 +1,15 @@
+import java.io.UnsupportedEncodingException;
+
 /**
  * Created by icejoywoo on 14-7-5.
  */
 public class HelloJava {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws UnsupportedEncodingException {
         System.out.println("Hello, World!");
+        System.out.println("단국대 홍보팀");
+        String a = "\202\071\204\063\202\067\202\071\206\067 \203\066\234\064\203\061\211\065\203\065";
+        String b = "단국대 홍보팀";
+        System.out.println(new String(a.getBytes(), "gb18030"));
+        System.out.println(new String(b.getBytes(), "gb18030").getBytes());
     }
 }
